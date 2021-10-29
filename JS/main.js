cae6fcb9f094d59afe8f254dfc23a1e6de3f14c1
Laurentiu <lcostin40@gmail.com>
2021-10-29 16:47:57 +0200

    Aggiunto codice js per cambiare img

--- JS/main.js
+++ JS/main.js
@@ -5,33 +5,63 @@ const vueApp = new Vue({
 
         images: [{
 
-            imma: "../img/01.jpg",
+            immagine: "../img/01.jpg",
             titolo: "Svezia",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
 
         },
         {
-            imma: "./img/02.jpg",
+            immagine: "./img/02.jpg",
             titolo: "Svizzera",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         },
         {
-            imma: "./img/03.jpg",
+            immagine: "./img/03.jpg",
             titolo: "Gran Bretagna",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         },
         {
-            imma: "./img/04.jpg",
+            immagine: "./img/04.jpg",
             titolo: "Germania",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         },
         {
-            imma: "./img/05.jpg",
+            immagine: "./img/05.jpg",
             titolo: "Paradise",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         }
         ],
+
         counter: 0,
+
+    },
+
+    methods: {
+        piu() {
+
+            ++this.counter
+
+            if (this.counter > this.images.length - 1) {
+                this.counter = 0
+            }
+
+        },
+
+        meno() {
+
+            --this.counter
+
+            if (this.counter < 0) {
+
+                this.counter = this.images.length - 1;
+            }
+
+        }
+
+
     }
 
+
+
 })
+
