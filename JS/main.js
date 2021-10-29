3f4aa45979c8bd28d540c94bd44f276137aacea0
Laurentiu <lcostin40@gmail.com>
2021-10-29 16:21:59 +0200

    Aggiunto img iniziale centrale ma si sono buggate le img laterali

--- JS/main.js
+++ JS/main.js
@@ -5,32 +5,33 @@ const vueApp = new Vue({
 
         images: [{
 
-            img: "../img/01.jpg",
+            imma: "../img/01.jpg",
             titolo: "Svezia",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
 
         },
         {
-            img: "./img/02.jpg",
+            imma: "./img/02.jpg",
             titolo: "Svizzera",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         },
         {
-            img: "./img/03.jpg",
+            imma: "./img/03.jpg",
             titolo: "Gran Bretagna",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         },
         {
-            img: "./img/04.jpg",
+            imma: "./img/04.jpg",
             titolo: "Germania",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
         },
         {
-            img: "./img/05.jpg",
+            imma: "./img/05.jpg",
             titolo: "Paradise",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
-        },
-        ]
+        }
+        ],
+        counter: 0,
     }
 
 })
