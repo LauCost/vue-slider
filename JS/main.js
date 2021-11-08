910f75beae31307244c634db85f28d24604f9bf9
Laurentiu <lcostin40@gmail.com>
2021-11-08 11:29:59 +0100

    Sistemano bug prima immagine

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,7 @@ const vueApp = new Vue({
 
         images: [{
 
-            immagine: "../img/01.jpg",
+            immagine: "./img/01.jpg",
             titolo: "Svezia",
             text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
 
